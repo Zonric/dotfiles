e2f706c759f3b2b39b023053ad4f9bc534d6f32b
Zonric <zonric42@gmail.com>
2023-01-27 12:06:03 -0500

    Herbstluft re-theme.

--- .config/herbstluftwm/pb_hlws.sh
+++ .config/herbstluftwm/pb_hlws.sh
@@ -1,81 +1,49 @@
-#!/usr/bin/env bash
-
-#  #A4B3B6  #D1E8E2  #88BDBC
-#  #65CCB8  #3AAFA9  #116466
-#  #254E58  #2C3531  #112D32
-#  #3C6334  #61892F  #86C232
-#  #FFCB9A  #D9B08C  #F16635
-
-#     歷     <|> 華
+#!/usr/bin/env sh
 
 monitor=${1:-0}
 tags=( $(herbstclient tag_status $monitor) )
 separator=""
 for i in ${tags[@]} ; do
   case ${i:1} in
-    '1')
-      icon="  "
-      ;;
-    '2')
-      icon="  "
-      ;;
-    '3')
-      icon="  "
-      ;;
-    '4')
-      icon="  "
-      ;;
-    '5')
-      icon=" 歷 "
-      ;;
-    '7')
-      icon="  "
-      ;;
-    '8')
-      icon="  "
-      ;;
-    '9')
-      icon="  "
-      ;;
-    '10')
-      icon="  "
-      ;;
-    *)
-      icon=" 華 "
-      ;;
+# --- ICONS:      歷     <|> 華
+    '1')  icon="󱃖" ;;
+    '2')  icon="" ;;
+    '3')  icon="" ;;
+    '4')  icon="" ;;
+    '5')  icon="歷";;
+    '6')  icon="" ;;
+    '7')  icon="" ;;
+    '8')  icon="" ;;
+    '9')  icon="󰙯" ;;
+    '10') icon="" ;;
+    *)    icon="華";;
   esac
   case ${i:0:1} in
-#    . the tag is empty
-#    : the tag is not empty
-#    + the tag is viewed on the specified MONITOR, but this monitor is not focused.
-#    # the tag is viewed on the specified MONITOR and it is focused.
-#    - the tag is viewed on a different MONITOR, but this monitor is not focused.
-#    % the tag is viewed on a different MONITOR and it is focused.
-#    ! the tag contains an urgent window
-    '.')
-      echo -n "%{B-}%{F-}%{A1:herbstclient focus_monitor $monitor && herbstclient us '${i:1}':} $icon %{A}%{F-}%{B-}"
-      ;;
-    ':')
-      echo -n "%{B#0C0032}%{F-}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}%{F-}%{B-}"
-      ;;
-    '+')
-      echo -n "%{B#190061}%{F-}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}%{F-}%{B-}"
-      ;;
-    '#')
-      echo -n "%{B#E67580}%{F#FFFFFF}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}%{F-}%{B-}"
-      ;;
-    '-' | '%')
-      echo -n "%{B#75B9E6}%{F-}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}%{F-}%{B-}"
-      ;;
-#    '%')
-#      echo -n "%{B#240090}%{F-}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}%{F-}%{B-}"
-#      ;;
+# --- Colors: gary [444444] white [ffffff] light green [66ff66] darkgreen [119966] light teal [33CCFF] dark teal [669999] yellow [FF00FF]
+# --- Tag is not displayed on a monitor
+    '.') # EMPTY (Dark Gray) [444444]
+      echo -n "%{B-}%{F#444444}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}" ;;
+    ':') # NOT EMPTY (White) [FFFFFF]
+      echo -n "%{B-}%{F#FFFFFF}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}" ;;
+
+# --- Tag is viewed on specified MONITOR
+    '#') # and is FOCUSED (Light Green) [66FF66]
+      echo -n "%{B-}%{F#66FF66}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}" ;;
+    '+') # and is NOT focused (Dark Green) [119966]
+      echo -n "%{B-}%{F#119966}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}" ;;
+
+# --- Tag is viewed on different monitor
+    '%') # and is FOCUSED (Light Blue Teal) [33CCFF]
+      echo -n "%{B-}%{F#33CCFF}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}" ;;
+    '-') # and is NOT focused (Dark Blue/Dark Teal) [669999]
+      echo -n "%{B-}%{F#669999}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}" ;;
+
+# --- Tag contains Urgent Window!
     '!')
-      echo -n "%{B-}%{F-}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}%{F-}%{B-}"
-      ;;
+      echo -n "%{B#660000}%{F#FF00FF}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}" ;;
+
+# --- Somethins gone wrong... Set font yellow      
     *)
-      echo -n "%{B-}%{F-}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}%{F-}%{B-}"
-      ;;
+      echo -n "%{B-}%{F#FFE135}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}%{F-}%{B-}" ;;
   esac
-  #echo -n $separator
 done
