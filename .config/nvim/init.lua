02e22e3e8339481b2f91f66261a48710374e8874
Zonric <zonric42@gmail.com>
2023-01-27 18:13:06 -0500

    Add buffer bar (tabs), and color highlighting (pigments)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -61,6 +61,59 @@ require('packer').startup(function(use)
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
 
+  -- Buffer bar
+  use {
+    'romgrk/barbar.nvim',
+    wants = 'nvim-web-devicons'
+  }
+
+  -- Color Highlight
+  use 'brenoprata10/nvim-highlight-colors'
+
+  local map = vim.api.nvim_set_keymap
+  local opts = { noremap = true, silent = true }
+
+  -- Move to previous/next
+  map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+  map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+  -- Re-order to previous/next
+  map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+  map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+  -- Goto buffer in position...
+  map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+  map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+  map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+  map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+  map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+  map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+  map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+  map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+  map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+  map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+  -- Pin/unpin buffer
+  map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+  -- Close buffer
+  map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+  -- Wipeout buffer
+  --                 :BufferWipeout
+  -- Close commands
+  --                 :BufferCloseAllButCurrent
+  --                 :BufferCloseAllButPinned
+  --                 :BufferCloseAllButCurrentOrPinned
+  --                 :BufferCloseBuffersLeft
+  --                 :BufferCloseBuffersRight
+  -- Magic buffer-picking mode
+  map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+  -- Sort automatically by...
+  map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+  map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+  map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+  map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+
+  -- Other:
+  -- :BarbarEnable - enables barbar (enabled by default)
+  -- :BarbarDisable - very bad command, should never be used
+
   -- Fuzzy Finder (files, lsp, etc)
   use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
 
@@ -399,6 +452,16 @@ require("nvim-tree").setup()
 
 vim.keymap.set('n', '<c-n>', ':NvimTreeToggle<CR>', {})
 vim.keymap.set('n', '<c-f>', ':NvimTreeFocus<CR>', {})
+
+vim.keymap.set('i', 'jl', '<esc>l', {})
+vim.keymap.set('v', 'jl', '<esc>', {})
+
+-- nvim-highlight-colors setup
+require("nvim-highlight-colors").setup {
+  render = 'background',
+  enable_named_colors = true,
+  enable_tailwind = true,
+}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
