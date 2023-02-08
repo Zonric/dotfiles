ccbb69df182f3ec83512522fd34976a703856e47
Zonric <zonric42@gmail.com>
2023-02-07 23:33:37 -0500

    Another dirty commit

--- .config/herbstluftwm/pb_hlws.sh
+++ .config/herbstluftwm/pb_hlws.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
 
+source ~/.config/herbstluftwm/themes/active_colors
+
+fcolor() {
+  echo -n "%{F$1}"
+}
+onclick() {
+  echo -n "%{A1:herbstclient focus_monitor $1 && herbstclient use '$2':}"
+}
+
 monitor=${1:-0}
 tags=( $(herbstclient tag_status $monitor) )
 separator=""
@@ -25,28 +34,52 @@ for i in ${tags[@]} ; do
 # --- Colors: gary [444444] white [ffffff] light green [66ff66] darkgreen [119966] light teal [33CCFF] dark teal [669999] yellow [FF00FF]
 # --- Tag is not displayed on a monitor
     '.') # EMPTY (Dark Gray) [444444]
-      echo -n "%{B-}%{F#444444}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}" ;;
+      fcolor $GRAY
+      onclick $monitor ${i:1}
+      echo -n " $icon %{A}"
+    ;;
     ':') # NOT EMPTY (White) [FFFFFF]
-      echo -n "%{B-}%{F#FFFFFF}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}" ;;
+      fcolor $FOREGROUND_ALT
+      onclick $monitor ${i:1}
+      echo -n " $icon %{A}"
+    ;;
 
 # --- Tag is viewed on specified MONITOR
     '#') # and is FOCUSED (Light Green) [66FF66]
-      echo -n "%{B-}%{F#66FF66}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}" ;;
+      fcolor $ACTIVE
+      onclick $monitor ${i:1}
+      echo -n " $icon %{A}"
+    ;;
     '+') # and is NOT focused (Dark Green) [119966]
-      echo -n "%{B-}%{F#119966}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}" ;;
+      fcolor $NORMAL_ALT
+      onclick $monitor ${i:1}
+      echo -n " $icon %{A}"
+    ;;
 
 # --- Tag is viewed on different monitor
     '%') # and is FOCUSED (Light Blue Teal) [33CCFF]
-      echo -n "%{B-}%{F#33CCFF}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}" ;;
+      fcolor $ACTIVE_ALT
+      onclick $monitor ${i:1}
+      echo -n " $icon %{A}"
+    ;;
     '-') # and is NOT focused (Dark Blue/Dark Teal) [669999]
-      echo -n "%{B-}%{F#669999}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}" ;;
+      fcolor $NORMAL
+      onclick $monitor ${i:1}
+      echo -n " $icon %{A}"
+    ;;
 
 # --- Tag contains Urgent Window!
     '!')
-      echo -n "%{B#660000}%{F#FF00FF}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}" ;;
+      fcolor $URGENT
+      onclick $monitor ${i:1}
+      echo -n " $icon %{A}"
+    ;;
 
 # --- Somethins gone wrong... Set font yellow      
     *)
-      echo -n "%{B-}%{F#FFE135}%{A1:herbstclient focus_monitor $monitor && herbstclient use '${i:1}':} $icon %{A}%{F-}%{B-}" ;;
+      fcolor $ERROR
+      onclick $monitor ${i:1}
+      echo -n " $icon %{A}"
+    ;;
   esac
 done
