a4bde87c1d05a1a7d6d9a98fa969c6f521bd9259
Zonric <zonric42@gmail.com>
2023-02-10 04:26:08 -0500

    Switch from xrandr script to xorg configs
    + 20-amdgpu.conf -> /etc/X11/xorg.conf.d/    # AMD TearFree Config
    + 30-monitors.conf -> /etc/X11/xorg.conf.d/  # Three monitor Config

--- .config/xorg/monitorsetup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-xrandr \
-  --output DisplayPort-1 --mode 1920x1080 --rate 144.00 --pos 0x0 --rotate normal \
-  --output DisplayPort-0 --mode 3440x1440 --rate 144.00 --right-of DisplayPort-1 --rotate normal --primary \
-  --output DisplayPort-2 --mode 1920x1080 --rate 144.00 --right-of DisplayPort-0 --rotate normal
-
